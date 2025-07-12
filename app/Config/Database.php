44ed0fccc51f6d383f5248605bf7f98a07c60834
pangeran-droid <pangeranxyz80@gmail.com>
2025-07-12 19:52:08 +0700

    update config

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'db_gis_perumahan',
+        'database'     => '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
